d1100bce7fa535bbc8f28825f6a5d4b30833b92b
Philippe M. Chiasson <gozer@mozilla.com>
2014-09-18 11:56:54 -0400

    massive change, break-out graphing in separate script

---
+++ tt/regenerate.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/env perl
+#
+use strict;
+use FindBin;
+
+use Template;
+
+my $tt = Template->new({
+               INCLUDE_PATH => "$FindBin::Bin/tt",  # or list ref
+               INTERPOLATE  => 1,               # expand "$var" in plain text
+               TRIM   => 1,               # cleanup whitespace
+               EVAL_PERL    => 1,               # evaluate Perl code blocks
+           });
+
+
+use Storable;
+my $tt_data = retrieve('index.storable');
+
+$tt->process('index.tt', $tt_data , 'index.html')
+               || die $tt->error(), "\n";
