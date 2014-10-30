c92ccafe2a1712636e7334c6a5010a410d5eafe4
Philippe M. Chiasson <gozer@mozilla.com>
2014-10-30 12:50:09 -0400

    pin at dist::zilla 5.020

--- build/jenkins.sh
+++ build/jenkins.sh
@@ -6,7 +6,8 @@ perlbrew init
 source ~/perl5/perlbrew/etc/bashrc
 perlbrew install-cpanm
 cpanm --local-lib=~/perl5 local::lib && eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib)
-cpanm Dist::Zilla
+# Dist::Zilla 5.022 seems broken
+cpanm Dist::Zilla@5.020
 
 dzil authordeps --missing | cpanm
 cpanm Test::Perl::Critic Archive::Tar::Wrapper
